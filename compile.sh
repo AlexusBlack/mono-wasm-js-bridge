4f59ecbf06d8f5c75ba3276434f7b01597412196
Alex Chernov <alexusblack@gmail.com>
2018-11-16 15:04:03 +1100

    using build folder during compilation process

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-csc /target:library -out:sample.dll /r:sdks/wasm-bcl/wasm/System.Net.Http.dll /r:sdks/framework/WebAssembly.Bindings.dll /r:sdks/framework/WebAssembly.Net.Http.dll dependency.cs sample.cs
+csc /target:library -out:build/sample.dll /r:sdks/wasm-bcl/wasm/System.Net.Http.dll /r:sdks/framework/WebAssembly.Bindings.dll /r:sdks/framework/WebAssembly.Net.Http.dll src/dependency.cs src/sample.cs
