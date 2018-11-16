f0782eaeb84daf7074c0305115e4e28faa001006
Alex Chernov <alexusblack@gmail.com>
2018-11-16 15:21:05 +1100

    new folder structure and package script

--- package.sh
+++ package.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-mono sdks/packager.exe --copy=always --out=./publish --asset=./sample.html --asset=server.py sample.dll
+
+#mono sdks/packager.exe --copy=always --out=./publish --asset=src/sample.html --asset=src/server.py build/sample.dll
+mono sdks/packager.exe --out=./publish build/sample.dll
+cp src/sample.html ./publish/
+cp src/server.py ./publish/
